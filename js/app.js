24138d42fc187a3e3122927bbed52fe3cab1860a
daisakuisa <daisaku.isahara@gmail.com>
2020-06-18 14:27:50 +0900

    commit3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-const img_src = ["img/photo01.jpg", "img/photo02.jpg", "img/photo03.jpg"];
+const img_src = [
+  "img/photo01.jpg",
+  "img/photo02.jpg",
+  "img/photo03.jpg",
+  "img/photo04.jpg",
+  "img/photo05.jpg",
+];
 let num = -1;
 
 function slideshow_timer() {
@@ -7,4 +13,4 @@ function slideshow_timer() {
   document.getElementById("img").src = img_src[num];
 }
 
-setInterval(slideshow_timer, 1000);
+setInterval(slideshow_timer, 3000);
